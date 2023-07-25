23f57b13ee356679a90d17468b8e2ae270cd5dda
Kolade Adelaiye <adelaiyekolade@yahoo.com>
2023-07-25 12:00:56 +0100

    Application displats scriptures via external file

---
+++ projects/random-scripture/main.js
@@ -0,0 +1,28 @@
+// Import all requred files and libraries
+import { defaultScriptures } from "./defaultScriptures.js";
+import "./setting.js";
+import { setup } from "./setting.js";
+
+// Grab all items needed to on the index page
+const button = document.querySelector("button");
+const cover = document.getElementById("cover")
+
+// Set all parameters by grabbing values setting file.
+cover.style.setProperty("background-color", setup.bgcolor)
+
+
+
+
+
+
+button.addEventListener("click", (event) => {
+    const randoNum = Math.floor(Math.random() * defaultScriptures.length);
+
+    let randyMe = defaultScriptures[randoNum];
+    let bibBook = randyMe[0];
+    let bookChapt = randyMe[1];
+    let chaptVrs = randyMe[2];
+    let scripture = randyMe[3];
+
+    document.getElementById("display").innerHTML = `<span id="bibBook"> ${bibBook} </span>  <span id="bookChapt"> ${bookChapt}: ${bookChapt} </span>  <br>${randyMe}`;
+});
---
+++ projects/random-scripture/setting.js
@@ -0,0 +1,1 @@
+export let setup = { source: "local", bgcolor: "purple" };
